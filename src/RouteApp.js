32a353bcdd939f4d27657917a8034f124c7191f1
DoanVanNghiep <doannghiepnd2k3@gmail.com>
2024-06-01 16:42:17 +0700

    xong del,view,add

--- src/RouteApp.js
+++ src/RouteApp.js
@@ -1,11 +1,14 @@
 import { useRoutes } from "react-router-dom";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import CreateEmployeeComponent from "./components/CreateEmployeeComponent";
+import ViewEmployeeComponent from "./components/ViewEmployeeComponent";
 
 const RouteApp = () => {
   let routes = useRoutes([
     { path: "/", element: <ListEmployeeComponent /> },
     { path: "/employee", element: <ListEmployeeComponent /> },
-    { path: "/view-employee/:id", element: <ListEmployeeComponent /> },
+    { path: "//add-employee/_add", element: < CreateEmployeeComponent/>},
+    { path: "/view-employee/:id", element: <ViewEmployeeComponent /> },
   ]);
   return routes;
 };
